f3f71e14bf76d22890de51d9f48a3c9a9732c5f1
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-11-17 16:45:55 +0200

    send successful request and fetching results from API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { debounce } from 'lodash';
 
+import { BASE_URL, API_KEY } from './constants';
 import SearchHeader from './containers/SearchHeader/SearchHeader';
 
 class App extends Component {
   state = {
-    value: '',
+    searchTerm: '',
     searchResults: []
+  };
+
+  debouncedSearch = debounce(this.fetchResults, 300);
+
+  async fetchResults(query) {
+    try {
+      const data = await axios.get(`
+        ${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}
+      `,
+      {
+        headers: {
+          'crossDomain': true,
+          'Content-Type': 'application/json'
+        }
+      });
+      this.setState({ searchResults: data.results });
+    } catch(err) {
+      console.log(err);
+    }
   }
 
   onSearch = e => {
-    this.setState({ value: e.target.value });
+    this.setState({ searchTerm: e.target.value }, () => {
+      if (this.state.searchTerm.trim().length > 0) {
+        this.debouncedSearch(this.state.searchTerm);
+      }
+    });
   }
 
   render() {
     return (
       <div className="App">
         <SearchHeader
-          value={this.state.value}
+          value={this.state.searchTerm}
           onSearch={this.onSearch}
         />
+        <div className="container">
+          Cards goes here.
+        </div>
       </div>
     );
   }
